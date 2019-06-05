b88e97f73cb9fba7074cce923dc7b808fdf244d8
Nicholas Ramsay <nick@yousponsor.me>
2019-06-05 17:28:03 +1000

    fixed up home page and readme text

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ I started this project with the intention of expanding my PHP knowledge from yea
 
 Currently, there is no db connection, however there is a mock retrieval system for '$posts'. I have done work with databases in PHP and as a result I didn't feel the need to practice it.
         
-This project uses proper routing (e.g. '/', '/posts', '/posts/1', ...) which I am not used to in PHP. Therefore, although simple, I am proud of this project, a lot has been learnt.
+This project uses proper routing (e.g. '/', '/posts', '/posts/1', ...) which I am not used to in PHP. Therefore, although simple, I am happy with the results of this project, a lot has been learnt.
 
 ## Observations
 * It is very easy to create ugly PHP code
--- views/home.view.php
+++ views/home.view.php
@@ -4,12 +4,14 @@
     <p >
         This project uses PHP with an MVC pattern and is styled with Bootstrap.
         <br><br>
+        I started this project with the intention of expanding my PHP knowledge from years ago by applying my back-end skills mostly from Node.js development(reason for MVC pattern).
+        <br><br>
         Currently, there is no db connection, however there is a mock retrieval system for '$posts'.
         I have done work with databases in PHP and as a result I didn't feel the need to practice it.
         <br><br>
-        This project uses proper routing (e.g. '/', '/posts', '/posts/1', ...) 
-        which I am not used to in PHP. Therefore, although simple, I am proud of this project, a lot has been learnt.
+        This project uses proper routing (e.g. '/', '/posts', '/posts/1', ...) which I am not used to in PHP. Therefore, although simple, I am happy with the results of this project, a lot has been learnt.
         <br><br>
         Please feel free to explore the website.
+
     </p>
 </div>  
